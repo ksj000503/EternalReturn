6709e4110818b99465a0ab69fe0f7710dbc40b98
ksj000503 <rlatjdwi0053@gmail.com>
2026-06-26 08:54:43 +0900

    seamliss

--- Source/EternalReturn/Private/Player/BasicPlayerState.cpp
+++ Source/EternalReturn/Private/Player/BasicPlayerState.cpp
@@ -15,8 +15,22 @@ void ABasicPlayerState::CopyProperties(APlayerState* PlayerState)
 
 	if (ABasicPlayerState* NewPS = Cast<ABasicPlayerState>(PlayerState))
 	{
+		// [로그 확인용] 에러(빨간색) 로그로 출력 창에 진짜 이 함수가 실행되는지 체크!
+		UE_LOG(LogTemp, Error, TEXT("!!복사 함수 작동 시작!! 기존 값: %s"), *SelectName.ToString());
+
+		// 클로드 말대로 this 빼고 깔끔하게 대입
 		NewPS->SelectName = SelectName;
+
+		UE_LOG(LogTemp, Error, TEXT("!!복사 완료!! 새 PlayerState에 들어간 값: %s"), *NewPS->SelectName.ToString());
 	}
+}
+
+void ABasicPlayerState::OnRep_SelectName()
+{
+	// 클라이언트나 리슨서버 화면에 값이 도착하면 이 로그가 찍힙니다.
+	UE_LOG(LogTemp, Warning, TEXT("클라이언트에 SelectName 동기화 완료: %s"), *SelectName.ToString());
+
+	// UI 업데이트나 캐릭터 변경 로직이 필요하다면 여기서 호출하세요!
 }
 
 void ABasicPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
