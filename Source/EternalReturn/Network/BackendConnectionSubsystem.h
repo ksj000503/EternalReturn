44b660d1e376ae17a36c55478d6ecdc45fb0286d
ksj000503 <rlatjdwi0053@gmail.com>
2026-07-15 17:45:14 +0900

    채팅 시스템 추가중

--- Source/EternalReturn/Network/BackendConnectionSubsystem.h
+++ Source/EternalReturn/Network/BackendConnectionSubsystem.h
@@ -34,6 +34,9 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Backend")
 	bool IsConnectedToBackend() const;
 
+	UFUNCTION(BlueprintCallable, Category = "Backend")
+	void SendMassage() const;
+
 	UPROPERTY(BlueprintAssignable, Category = "Backend")
 	FOnBackendMessageReceived OnMessageReceived;
 
